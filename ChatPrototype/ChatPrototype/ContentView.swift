d6c155bd576babe1cb392fd8290e3e6e6d0decd2
Elliot Perreault <168759915+elliotperreault@users.noreply.github.com>
2025-02-14 23:16:06 -0500

    created full chat

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -12,10 +12,11 @@ struct TextStyleModifier: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .font(.title)
+            .font(.title2)
             .padding()
             .background(backgroundColor, in: RoundedRectangle(cornerRadius: 10))
             .foregroundColor(.white)
+            .padding(5)
     }
 }
 
@@ -28,15 +29,41 @@ extension View {
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        @State var messages: [String] = [
+            "Salut, ça va ?",
+            "Ça va bien et toi ?",
+            "Allo"
+        ]
+        
+        VStack(alignment: .leading)
+        {
             Image(systemName: "message")
-                .imageScale(.large)
+                .font(.system(size: 60))
                 .foregroundStyle(.tint)
-            Text("Salut, ça va ?")
-                .applyTextStyle(backgroundColor: Color.blue)
-            Text("Ça va bien et toi ?")
-                .applyTextStyle(backgroundColor: Color.gray)
+                .frame(maxWidth: .infinity, alignment: .center)
+            Divider()
+            Spacer()
+            ForEach(messages, id: \.self) { message in
+                            Text(message)
+                                .applyTextStyle(backgroundColor: message == "Salut, ça va ?" ? Color.blue : Color.gray)
+                                .frame(maxWidth: .infinity, alignment: message == "Salut, ça va ?" ? .leading : .trailing)
+                }
+            Divider()
             
+            HStack {
+                TextField("Message", text: .constant(""))
+                    .font(.title)
+                Image(systemName: "paperplane")
+                    .font(.system(size: 24))
+                    .foregroundStyle(.tint)
+                    .onTapGesture {
+                        messages.append("New blue message!")
+                    }
+                   
+            }.overlay(Rectangle()
+                .frame(height: 2)
+                .foregroundColor(.gray), alignment: .bottom)
+            .padding()
         }
     }
 }
