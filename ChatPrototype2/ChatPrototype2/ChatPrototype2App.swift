6dbe7e8f640276feb4e8dab9ad6cae35259012f2
Elliot Perreault <168759915+elliotperreault@users.noreply.github.com>
2025-02-19 20:53:09 -0500

    added second example project

---
+++ ChatPrototype2/ChatPrototype2/ChatPrototype2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototype2App.swift
+//  ChatPrototype2
+//
+//  Created by Elliot Perreault on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototype2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototype2/ChatPrototype2Tests/ChatPrototype2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototype2Tests.swift
+//  ChatPrototype2Tests
+//
+//  Created by Elliot Perreault on 2025-02-19.
+//
+
+import Testing
+@testable import ChatPrototype2
+
+struct ChatPrototype2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
